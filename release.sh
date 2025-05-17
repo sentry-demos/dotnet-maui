e4b323e5e45af604bc71978bd9e7ba767e10d1d9
Bruno Garcia <bruno@brunogarcia.com>
2025-05-16 21:05:35 -0400

    Update release.sh

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dotnet build DotNetMaui.csproj -t:Build -f net9.0-ios -r iossimulator-x64
+dotnet build EmpowerPlant.csproj -t:Build -f net9.0-ios -r iossimulator-x64
 zip -r dotnet-maui-ios-x64.zip bin/Debug/net9.0-ios
 
-dotnet publish DotNetMaui.csproj -f net9.0-android
+dotnet publish EmpowerPlant.csproj -f net9.0-android
 cp bin/Release/net9.0-android/publish/io.sentry.dotnet.maui.empowerplant-Signed.apk dotnet-maui-android.apk
